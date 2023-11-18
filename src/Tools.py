759e02b8c1b561a5813b63da835f9007cceef930
Leo Walker <leowalker89@gmail.com>
2023-11-17 16:14:29 -0800

    updated

--- src/Tools.py
+++ src/Tools.py
@@ -1,4 +1,91 @@
-def analyze_resume(full_text, model='gpt-3.5-turbo-1106'):
+import streamlit as st
+import openai
+from langchain.document_loaders import PyPDFLoader
+from dotenv import load_dotenv
+import os
+import openai
+import PyPDF2
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI, ChatAnthropic
+import json
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import ValidationError
+from json import JSONDecodeError
+
+# Load environment variables containing API keys
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+os.environ["LANGCHAIN_API_KEY"] = str(os.getenv("LANGCHAIN_API_KEY"))
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_PROJECT"] = "elevated_ambitions_extraction"
+
+def pdf_to_string(resume_pdf):
+    with open('../pdf/Leo_Walker_Breakline_Resume.pdf', 'rb') as f:
+        pdf_reader = PyPDF2.PdfReader(f)
+        num_pages = len(pdf_reader.pages)
+        text = ''
+        for i in range(num_pages):
+            page = pdf_reader.pages[i]
+            text += page.extract_text()
+        return text
+
+from resume_template import Resume
+
+def extract_resume_fields(full_text, model='gpt-3.5-turbo-1106'):
+    """
+    Analyze a resume text and extract structured information using a specified language model.
+
+    Parameters:
+    full_text (str): The text content of the resume.
+    model (str): The language model to use for processing the text.
+
+    Returns:
+    dict: A dictionary containing structured information extracted from the resume.
+    """
+    # The Resume object is imported from the local resume_template file
+
+    with open("../prompts/resume_extraction.prompt", "r") as f:
+            template = f.read()
+
+    parser = PydanticOutputParser(pydantic_object=Resume)
+
+    prompt_template = PromptTemplate(
+        template=template,
+        input_variables=["resume"],
+        partial_variables={"response_template": parser.get_format_instructions()},
+    )
+
+    # Invoke the language model and process the resume
+    formatted_input = prompt_template.format_prompt(resume=full_text)
+    # llm = ChatOpenAI(temperature=0, model=model)
+    # output = llm.invoke(formatted_input.to_string())
+    if model in ["gpt-4-1106-preview", "gpt-4", "gpt-3.5-turbo-1106"]:
+        llm = ChatOpenAI(temperature=0, model=model)
+        
+    elif model in ["claude-2", "claude-instant-1"]:
+        llm = ChatAnthropic(model=model, max_tokens=20_000)
+    
+    else:
+        llm = ChatOpenAI(temperature=0, model=model)
+    
+    try:
+        output = llm.invoke(formatted_input.to_string())
+        parsed_output = parser.parse(output.content)
+        return parsed_output
+    
+    except ValidationError as e:
+        print((f"Validation error: {e}"))
+        print(output)
+        return output.content
+    
+    except JSONDecodeError as e:
+        print((f"JSONDecodeError error: {e}"))
+        print(output)
+        return output.content
+
+def extract_resume_fields_json(full_text, model='gpt-3.5-turbo-1106'):
     """
     Analyze a resume text and extract structured information using a specified language model.
 
@@ -56,24 +143,26 @@ def upgrade_resume_bullets(extracted_resume):
     Iterate through the work experience in a resume and upgrade each bullet point.
 
     Parameters:
-    extracted_resume (dict): A dictionary containing a structured resume.
+    extracted_resume (Resume): A Resume object containing a structured resume.
 
     Returns:
-    dict: The resume dictionary with enhanced bullet points in the work experience section.
+    Resume: The Resume object with enhanced bullet points in the work experience section.
     """
     # Enhance bullet points for each work experience entry
-    for experience in extracted_resume['work_experience']:
-        experience_desc = ' '.join([experience[field] for field in ['company', 'title', 'duration', 'description']])
-        # skip experience if there is no achievements
-        if not experience['achievements']:
+    for experience in extracted_resume.work_experience:
+        experience_desc = ' '.join([str(getattr(experience, field)) for field in ['company', 'title', 'duration', 'description']])
+        # skip experience if there are no notable contributions
+        if not experience.notable_contributions:
             continue
         else:
-            for i, bullet in enumerate(experience['achievements']):
-                # skip bullet if it is empty
-                if bullet == '':
+            for i, contribution in enumerate(experience.notable_contributions):
+                # skip contribution if it is empty
+                if contribution.notable_contribution == '':
                     continue
                 else:
-                    experience['achievements'][i] = upgrade_experience_bullet(experience_desc, bullet)
+                    enhanced_contribution = upgrade_experience_bullet(experience_desc, contribution.notable_contribution)
+                    # Update the notable contribution with the enhanced version
+                    experience.notable_contributions[i].notable_contribution = enhanced_contribution
 
     return extracted_resume
 
@@ -131,3 +220,6 @@ def generate_synthetic_responses(user_profile, questions, model='gpt-3.5-turbo-1
     response = chat_llm.invoke(formatted_input)
 
     return json.loads(response.content)
+
+if __name__ == '__main__':
+
